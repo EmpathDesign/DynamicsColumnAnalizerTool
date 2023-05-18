7c09811b40f593eb719b4232a3146fae6db923cb
Patrick Mackay <patrickmackay68@gmail.com>
2023-05-18 15:07:43 -0500

    First (hopefully) functional copy
    lots of small bugfixes
    added prompts for filepaths for schema and data
    time to test it against the actual excels

--- DynamicsColumnAnalizerTool/Classes.cs
+++ DynamicsColumnAnalizerTool/Classes.cs
@@ -1,21 +1,22 @@
 ﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing.Charts;
 
 namespace Classes;
 
 public class Column
 {
     //data from the schema
-    public string DisplayName { get; set; } 
-    public string LogicalName { get; set; }
-    public string SchemaName { get; set; }
-    public string AttributeType { get; set; } //the datatype (can be Whole Number, Lookup, Text, ect)
-    public string Type { get; set; } //simple / rollup (possibly more)
-    public bool HasCustomAttribute { get; set; } 
-    public string AdditionalData { get; set; } 
-    public string Description { get; set; } 
+    public string? DisplayName { get; set; }
+    public string? LogicalName { get; set; }
+    public string? SchemaName { get; set; }
+    public string? AttributeType { get; set; } //the datatype (can be Whole Number, Lookup, Text, ect)
+    public string? Type { get; set; } //simple / rollup (possibly more)
+    public bool? HasCustomAttribute { get; set; } 
+    public string? AdditionalData { get; set; } 
+    public string? Description { get; set; } 
 
     public IXLColumn ColumnData { get; set; } //IXLColumn from IXLWorksheet
-    public int NumFilledRows { get; } //number of filled rows in the column
+    public int? NumFilledRows { get; } //number of filled rows in the column
     public int NumRows { get; set; }
     public int SchemaRow { get; set; } //row on the schema table
     public string PercentFilled { get; }
@@ -25,15 +26,17 @@ public class Column
         ColumnData = col;
         
         NumFilledRows = findNumFulledRows();
-        PercentFilled = ((float)NumFilledRows / (float)NumRows).ToString("#.##") + "%"
+        PercentFilled = ((float)NumFilledRows / (float)NumRows).ToString("#.##") + "%";
 
+        //define schema row here to avoid non nullable / nullable type errors, if for whatever reason this value does not get reset from 0 the process will crash
+        SchemaRow = 0;
         //adjust the width of the column to match its contents (no more cutoff column names)
         col.AdjustToContents();
     }
 
     public string cell(int row)
     {
-        return ColumnData.Cell(row);
+        return ColumnData.Cell(row).Value.ToString();
     }
 
     //gets total rows with data (ignores whitespaces)
@@ -56,21 +59,21 @@ public class Column
 public class Table
 {
     //data from the schema
-    public string PluralDisplayName { get; set; }
-    public string Entity { get; set; }
-    public string Description { get; set; }
-    public string SchemaName { get; set; }
-    public string LogicalName { get; set; }
-    public int ObjectTypeCode { get; set; }
-    public bool IsCustomEntity { get; set; }
-    public string OwnershipType { get; set; }
+    public string? PluralDisplayName { get; set; }
+    public string? Entity { get; set; }
+    public string? Description { get; set; }
+    public string? SchemaName { get; set; }
+    public string? LogicalName { get; set; }
+    public int? ObjectTypeCode { get; set; }
+    public bool? IsCustomEntity { get; set; }
+    public string? OwnershipType { get; set; }
 
     
     public IXLWorksheet Schema { get; } //schema worksheet, rotated "90 degees" (column names iterate by row instead of by column)
     public List<Column> Columns { get; set; } //list of columns, Column[index].ColumnData.Cell(row) OR Column[index].Cell(row)
-    public int NumRows { get; } //number of data entries, used to analyze the usefulnes of any column or table
-    public int NumCols { get; } //number of columns in table
-    public DateTime LastUpdated { get; } //last time the table was updated, not all tables have this
+    public int? NumRows { get; } //number of data entries, used to analyze the usefulnes of any column or table
+    public int? NumCols { get; } //number of columns in table
+    public DateTime? LastUpdated { get; } //last time the table was updated, not all tables have this
 
     public Table(IXLWorkbook dataTable, IXLWorkbook schema)
     {
@@ -81,14 +84,14 @@ public class Table
         Description = Schema.Cell(3, 2).Value.ToString();
         SchemaName = Schema.Cell(4, 2).Value.ToString();
         LogicalName = Schema.Cell(5, 2).Value.ToString();
-        ObjectTypeCode = Int32.Parse(Schema.Cell(6, 2).Value);
-        IsCustomEntity = Boolean.Parse(Schema.Cell(7, 2).Value);
+        ObjectTypeCode = Int32.Parse(Schema.Cell(6, 2).Value.ToString());
+        IsCustomEntity = Boolean.Parse(Schema.Cell(7, 2).Value.ToString());
         OwnershipType = Schema.Cell(8, 2).Value.ToString();
 
         //add IXLColumns to Columns list
         foreach (IXLColumn col in data.Columns())
         {
-            Columns.Add(new Column(col))
+            Columns.Add(new Column(col));
         }
        
         NumCols = Columns.Count;
@@ -101,14 +104,16 @@ public class Table
             col.DisplayName = col.cell(1); //get displayname from the top of the column
             col.SchemaRow = findMatchingSchemaRow(col.DisplayName); //gets the row in which the column metadata is stored in the schema file
             //gets all other column data 
-            col.LogicalName = schema.Cell(col.schemaRow, 1).Value.ToString();  
-            col.SchemaName = schema.Cell(col.schemaRow, 2).Value.ToString();
-            col.AttributeType = schema.Cell(col.schemaRow, 4).Value.ToString();
-            col.Description = schema.Cell(col.schemaRow, 5).Value.ToString();
-            col.HasCustomAttribute = Boolean.Parse(schema.Cell(col.schemaRow, 6).Value);
-            col.Type = schema.Cell(col.schemaRow, 7).Value.ToString();
-            col.AdditionalData = schema.Cell(col.schemaRow, 8).Value.ToString();
+            col.LogicalName = Schema.Cell(col.SchemaRow, 1).Value.ToString();  
+            col.SchemaName = Schema.Cell(col.SchemaRow, 2).Value.ToString();
+            col.AttributeType = Schema.Cell(col.SchemaRow, 4).Value.ToString();
+            col.Description = Schema.Cell(col.SchemaRow, 5).Value.ToString();
+            col.HasCustomAttribute = Boolean.Parse(Schema.Cell(col.SchemaRow, 6).Value.ToString());
+            col.Type = Schema.Cell(col.SchemaRow, 7).Value.ToString();
+            col.AdditionalData = Schema.Cell(col.SchemaRow, 8).Value.ToString();
         }
+
+        
     }
     
     //assumes the Display Names are on column 3
@@ -116,13 +121,16 @@ public class Table
     {
         //skips the first ten rows
         //compares display name in schema to display name stored in the Column class
-        for (int i = 10; i < MaxRow; i++)
+        for (int i = 10; i < NumRows; i++)
         {
             if(Schema.Cell(i, 3).Value.ToString().Equals(columnDisplayName))
             {
                 return i;
             }
         }
+
+        Console.WriteLine("Oh No! It looks like no schema row was found for " + columnDisplayName);
+        return 0; //if no row is found return 0
     }
 
 
@@ -151,28 +159,45 @@ public class Table
 
     }
     */
+
+
     public void analyzeTable()
     {
-        //copy paste old code from v2 to analyze the table
+        //backtraces? to the workbook and makes a new worksheet for output
+        IXLWorksheet output = Columns[1].ColumnData.Worksheet.Workbook.AddWorksheet();
+        output.Cell(1, 2).Value = "Column Names";
+        output.Cell(1, 3).Value = "Total Filled";
+        output.Cell(1, 4).Value = "Percent Filled";
+
+        for (int i = 1; i < NumCols; i++)
+        {
+            output.Cell(i, 1).Value = Columns[i].DisplayName;
+            output.Cell(i, 2).Value = Columns[i].NumFilledRows;
+            output.Cell(i, 3).Value = Columns[i].DisplayName;
+
+        }
+
+
+        //this might not actually save 
+        Columns[1].ColumnData.Worksheet.Workbook.Save();
     }
 
     //get the data from a specific cell, because Columns is a list, do col-1 otherwise if a user tries to access cell(row, 1) it will return the second column not the first, row should never be below 1 either 
     public string cell(int row, int col)
     {
-        return Columns[col-1].cell(row).Value.ToString()
+        return Columns[col - 1].cell(row);
     }
 
-    //internal method for finding the max row in a data table
+    //internal method for finding the max row in a excel
     private int findNumRow()
     {
         int num = 0;
-        // iterate through column (objects) 
         foreach (Column col in Columns)
         {
             num = 0;
             //while the cell in that column in that row is not empty iterate i and num 
             //this code assumes that there is at least one column that is fully populated (100% of rows filled)
-            for (int i = 1; !string.IsNullOrWhiteSpace(col.cell(i).Value.ToString()); i++)
+            for (int i = 1; !string.IsNullOrWhiteSpace(col.cell(i)); i++)
             {
                 num++;
             }
--- DynamicsColumnAnalizerTool/Program.cs
+++ DynamicsColumnAnalizerTool/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ClosedXML;
 using Classes;
+using ClosedXML.Excel;
 
 namespace Program
 {
@@ -8,6 +9,17 @@ namespace Program
     {
         public static void Main(string[] args)
         {
+            //get path to xlsx files
+            Console.WriteLine("""Select An Inactive XLSX File   (format example: C\\Users\\user\\Desktop\\xlsxFile.xlsx)""");
+            string dataPath = Console.ReadLine()+""; //concat for non null
+            Console.WriteLine("""Select An Inactive XLSX Schema File   (format example: C\\Users\\user\\Desktop\\xlsxSchemaFile.xlsx)""");
+            string schemaPath = Console.ReadLine() + ""; //concat for non null
+            Console.WriteLine(dataPath + " & " + schemaPath);
+            IXLWorkbook wb = new XLWorkbook(dataPath);
+            IXLWorkbook schemaWb = new XLWorkbook(schemaPath);
+            Table table = new Table(wb, schemaWb);
+            //pause so i can read the console
+            System.Threading.Thread.Sleep(5000);
 
         }
     }
